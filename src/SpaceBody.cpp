cca8e2f7d0abf70044fefce7f5c1b3771f456f7a
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2020-01-31 14:03:19 +0300

    Added imaginary projection movement

--- src/SpaceBody.cpp
+++ src/SpaceBody.cpp
@@ -11,8 +11,6 @@ SpaceBody::SpaceBody(sf::Vector2f pos, sf::Vector2f speed, sf::Vector2f accelera
     mAcceleration = acceleration;
     mProjection = sprite;
     mProjection.setScale(7, 7);
-    mProjection.setOrigin(mProjection.getTexture()->getSize().x / 2,
-                          mProjection.getTexture()->getSize().y / 2);
     mImaginaryProjection = sf::Sprite(mProjection);
 }
 
@@ -26,33 +24,36 @@ void SpaceBody::move(sf::Time time, sf::Vector2u sceneSize) {
     mPosition += sf::Vector2f(distanceX, distanceY);
 
     sf::Vector2f imaginaryPosition(mPosition);
-    sf::Vector2u textureSize = mImaginaryProjection.getTexture()->getSize();
-
-    sf::FloatRect commonArea;
-    mImaginaryProjection.getGlobalBounds().intersects(sf::FloatRect(0, 0, sceneSize.x, sceneSize.y), commonArea);
-
-    if (commonArea.height < textureSize.y) {
-        imaginaryPosition.y = sceneSize.y + mPosition.y;
-    }
-    if (commonArea.width < textureSize.x) {
-        imaginaryPosition.x = sceneSize.x - mPosition.x;
-    }
-
+    sf::Vector2u textureSize;
+    textureSize.x = mImaginaryProjection.getTexture()->getSize().x * mImaginaryProjection.getScale().x;
+    textureSize.y = mImaginaryProjection.getTexture()->getSize().y * mImaginaryProjection.getScale().y;
 
     if (mPosition.x > sceneSize.x) {
         mPosition.x -= int(mPosition.x / sceneSize.x) * sceneSize.x;
-    } else if (mPosition.x < 0) {
+    } else if (mPosition.x < -int(textureSize.y)) {
         mPosition.x -= int(mPosition.x / sceneSize.x) * sceneSize.x;
         mPosition.x += sceneSize.x;
     }
     if (mPosition.y > sceneSize.y) {
         mPosition.y -= int(mPosition.y / sceneSize.y) * sceneSize.y;
-    } else if (mPosition.y < 0) {
+    } else if (mPosition.y < -int(textureSize.y)) {
         mPosition.y -= int(mPosition.y / sceneSize.y) * sceneSize.y;
         mPosition.y += sceneSize.y;
     }
+    // locating imaginary projection
+    if (mPosition.y < 0) {
+        imaginaryPosition.y = sceneSize.y + mPosition.y;
+    } else if (mPosition.y > sceneSize.y - textureSize.y) {
+        imaginaryPosition.y = sceneSize.y - mPosition.y;
+    }
+    if (mPosition.x < 0) {
+        imaginaryPosition.x = sceneSize.x + mPosition.x;
+    } else if (mPosition.x > sceneSize.x - textureSize.x) {
+        imaginaryPosition.x = sceneSize.x - mPosition.x;
+    }
 
     //TODO add imaginary projection
+//    std::cout << sceneSize.x << ' ' << sceneSize.y << " im " << imaginaryPosition.x <<' ' << imaginaryPosition.y <<" real " << mPosition.x << ' '<< mPosition.y<< '\n';
     mImaginaryProjection.setPosition(imaginaryPosition);
     mProjection.setPosition(mPosition);
 }
